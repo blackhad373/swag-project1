ce06bc523f75323d4da71fdfe033054e0a967515
blackhad373 <113373296+blackhad373@users.noreply.github.com>
2025-02-02 23:07:06 +0530

    added condition

--- tests/Pages/login.page.js
+++ tests/Pages/login.page.js
@@ -1,3 +1,4 @@
+const {expect} = require('@Playwright/test')
 class LoginPage {
     constructor(page) {
       this.page = page
@@ -14,19 +15,18 @@ class LoginPage {
       this.continue = page.locator('#continue')
       this.completebuying = page.locator('#finish')
     }
-    // -----------------------------Login with correct creds----------------------------------------
+   // -----------------------------Login with correct creds----------------------------------------
 
     async Correctlogincreds(){
-     let titlematch = 'Swag Labs'
+    
       const logoname = 'Swag Labs'
       await this.page.goto('https://www.saucedemo.com/')
+      await expect(this.page).toHaveURL(/saucedemo/);      //To check if the url is correct or not
+      console.log('URL Matched');  
       const title = await this.page.title()                               //To get the title of the page
       console.log(title)                                                  //Printing the title of the page
-      // if(await expect(title).toHaveTitle("Swag Labs")){ 
-      //   console.log('Title Matched')
-      // }else{
-      //   console.log('Title Mismatch')
-      // }
+      await expect(this.page).toHaveTitle("Swag Labs")
+      console.log('Title Matched')
       await this.Username.fill('standard_user')                             //To fill the username field
       await this.Password.fill('secret_sauce')                              //To fill the password field
       await this.Submit.click()                                             //To click on the submit button        
@@ -55,12 +55,31 @@ class LoginPage {
       await this.Correctlogincreds()                                             //calling the Correctlogincreds function to login
       await this.addbackpackpage.click()                                        //To add the backpack to the cart
       await this.addbikelight.click()                                           //To add the bike light to the cart
-      await this.cart.click()                                                   //To click on the cart icon
-      await this.checkout.click()                                               //To click on the checkout button
+      await this.cart.click()   
+      await expect(this.page).toHaveURL(/cart.html/)                            //To check if the url is correct or not
+      console.log("Redirect to Your Cart Section")                                             //To click on the cart icon
+      await this.checkout.click()   
+      await expect(this.page).toHaveURL(/checkout/)                            //To check if the url is correct or not
+      console.log("Redirect to Your Your Information Section")                  //To click on the checkout button
       await this.firstname.fill('John')                                         //To fill the first name field for buying the product
       await this.lastname.fill('Doe')                                           //To fill the last name field for buying the product
       await this.postalcode.fill('12345')                                       //To fill the postal code field for buying the product
-      await this.continue.click()                                               //To click on the continue button and redirect to next page
+      await this.continue.click()                                                //To click on the continue button and redirect to next page
+      const product1 = await this.page.locator(".inventory_item_price").first().textContent()   //To get the price of the first product
+      console.log(product1) 
+      const product2 = await this.page.locator(".inventory_item_price").last().textContent()    //To get the price of the second product
+      console.log(product2) 
+      let TotalPrice  = parseFloat(product1.slice(1)) + parseFloat(product2.slice(1))           //To calculate the total price of the products
+      console.log("Total Price",+TotalPrice)
+      const Price = await this.page.locator('.summary_subtotal_label').textContent()
+      console.log(Price)
+     const FinalPrice = parseFloat(Price.slice(13))                                      
+      console.log(FinalPrice)
+      if(TotalPrice == FinalPrice){                                            //To check if the total price of the products is matched or not
+          console.log('Total Price Matched')}
+      else{
+          console.log('Total Price Not Matched')
+      }
       await this.completebuying.click()                                         //To click on the finish button and complete the buying process
       console.log(await this.page.locator('.complete-header').textContent())    //To check if the buying process is completed or not
   }
